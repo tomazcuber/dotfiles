aecdfc22a068382f6b5ce21f572398e2b54e857d
lisahtoledo <elisartoledo@gmail.com>
2019-08-17 23:12:23 -0300

    Adds instalation script with .vimrc symlinking.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Installation script for tomazcuber's custom dotfiles. 
+#--------------------------------------------------------------
+#Create Symbolic Links
+_dir="$PWD"
+ln -sv "$_dir/vim/.vimrc" ~ 
+
